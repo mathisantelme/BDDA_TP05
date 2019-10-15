6501a3ccf0e651fe404904ead8050ffddffa6da2
Mathis Antelme <mathis.antelme@gmail.com>
2019-10-15 10:29:34 +0200

    Implementation of interface DomainObject by class Book

--- project/DM_etud/src/main/java/dm/Book.java
+++ project/DM_etud/src/main/java/dm/Book.java
@@ -3,6 +3,23 @@ package dm;
 /**
  * Domain class representing a book
  */
-public class Book {
-    /* TO COMPLETE */
+public class Book implements DomainObject {
+    // Variables
+    private Object ID;
+
+    // Constructor
+    public Book(Object ID) {
+        this.ID = ID;
+    }
+
+    // Methods
+    @Override
+    public Object getId() {
+        return this.ID;
+    }
+
+    @Override
+    public void setId(Object p_ID) {
+        this.ID = p_ID;
+    }
 }
