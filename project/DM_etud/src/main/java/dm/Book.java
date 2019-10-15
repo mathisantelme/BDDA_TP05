8a175ac00eea511e28823db452e15fba5a24889d
mathisantelme <mathis.antelme@gmail.com>
2019-10-15 11:05:34 +0200

    Completed the Book object

--- project/DM_etud/src/main/java/dm/Book.java
+++ project/DM_etud/src/main/java/dm/Book.java
@@ -5,21 +5,48 @@ package dm;
  */
 public class Book implements DomainObject {
     // Variables
-    private Object ID;
+    private String ID;
+    private String title;
+    private String author;
 
     // Constructor
-    public Book(Object ID) {
-        this.ID = ID;
+    public Book(String ID, String p_title, String p_author) throws IllegalArgumentException {
+        if (ID != null)
+            this.ID = ID;
+        else
+            throw new IllegalArgumentException("Cannot use null ID");
+
+        this.title = p_title;
+        this.author = p_author;
     }
 
-    // Methods
+    // Accessors
     @Override
     public Object getId() {
         return this.ID;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
     @Override
-    public void setId(Object p_ID) {
-        this.ID = p_ID;
+    public void setId (Object p_ID) throws IllegalArgumentException {
+        if (ID != null)
+            this.ID = ID;
+        else
+            throw new IllegalArgumentException("Cannot use null ID");
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 }
